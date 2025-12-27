86428e1c443f93810b1deae8ed77c4bbe05e21c4
Alex Kwiatkowski <alex+git@fremantle.io>
2025-12-26 22:00:16 -0800

    feat(tunnel): add RestartPolicy for process supervision

--- vibes-core/src/tunnel/mod.rs
+++ vibes-core/src/tunnel/mod.rs
@@ -2,8 +2,10 @@
 
 pub mod cloudflared;
 pub mod config;
+pub mod restart;
 pub mod state;
 
 pub use cloudflared::{check_installation, CloudflaredInfo};
 pub use config::{TunnelConfig, TunnelMode};
+pub use restart::RestartPolicy;
 pub use state::{LogLevel, TunnelEvent, TunnelState};
